86095204ff9642877bd53a4a7de4ef4ac404ad05
Hemant <hemantsaini95689@gmail.com>
2017-05-29 23:06:02 +0530

    set Animations in Panel

--- src/Ellio.java
+++ src/Ellio.java
@@ -1,22 +1,43 @@
 import frames.EllioFrame;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 /**
  * Created by Hemant Saini on 29-05-2017.
  */
-public class Ellio {
+public class Ellio implements KeyListener {
     public static void main(String[] args) {
 
         EllioFrame ellioFrame = new EllioFrame();
+
 
         while (true) {
             ellioFrame.getContentPane().repaint();
 //            ellioFrame.repaint(); TODO check which one is convenient
             try {
-                Thread.sleep(20);
+                Thread.sleep(30);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.print("abefe");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.print("abefe");
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.print("abefe");
 
     }
 }
--- src/panels/MainAnimationPanel.java
+++ src/panels/MainAnimationPanel.java
@@ -1,29 +1,28 @@
 package panels;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.util.Random;
 
 /**
  * Created by Hemant Saini on 29-05-2017.
  */
 public class MainAnimationPanel extends EllioPanel implements KeyListener, MouseListener {
 
-    private static final int cloud1X = 400;
-    private static final int cloud1Y = 150;
-    private static final int cloud2X = 900;
-    private static final int cloud2Y = 70;
-    private static final int playerY = 315;
-    private static final boolean cloud1Visible = true;
-    private static final Color skyBlueColor = new Color(208, 244, 247);
+    private static final int PLAYER_Y = 312;
 
+    private int cloud1X = 400;
+    private int cloud1Y = 150;
+    private int cloud2X = 900;
+    private int cloud2Y = 70;
+    private int playerY = PLAYER_Y;
     private int playerVelocityY = 0;
     private int playerAccY = 0;
+    private boolean cloud1Visible = true;
     private boolean isPaused = false;
 
 
@@ -38,11 +37,26 @@ public class MainAnimationPanel extends EllioPanel implements KeyListener, Mouse
     private BufferedImage cloud2Image = MainAnimationPanel.loadImage("cloud2.png");
     private BufferedImage playerImages[] = {image1, image2, image3, image4, image5, image4, image3, image2};
 
+    private int counter = 0;
+    private Rectangle playerRectangle;
+    private Rectangle cloud1Rectangle;
+    private Random generator = new Random();
+
+
+    public MainAnimationPanel() {
+        setFocusable(true);
+        requestFocusInWindow();
+        addKeyListener(this);
+        addMouseListener(this);
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        System.out.println("printComponent Called at MainAnimationPanel");
+        animatePlayer();
+        animateCloud();
+        animateHurdles();
+        hideObjects();
         g.setColor(Color.blue);
         g.fillRect(0, 0, 800, 450);
         if (this.cloud1Visible) {
@@ -53,23 +67,85 @@ public class MainAnimationPanel extends EllioPanel implements KeyListener, Mouse
         g.drawImage(grassImage, 0, 450 - 45, null);
     }
 
+    private void hideObjects() {
+        if (playerRectangle.intersects(cloud1Rectangle)) {
+            cloud1Visible = false;
+        }
+    }
+
+    private void animateHurdles() {
+
+    }
+
+    private void animateCloud() {
+        if (playerY >= PLAYER_Y) {
+            playerY = PLAYER_Y;
+            playerVelocityY = 0;
+            playerAccY = 0;
+        }
+
+        if (cloud1X <= -100) {
+            cloud1Y = generator.nextInt(190) + 10;
+            cloud1X = 900;
+            cloud1Visible = true;
+        }
+
+        if (cloud2X <= -100) {
+            cloud2Y = generator.nextInt(190) + 10;
+            cloud2X = 900;
+        }
+
+        cloud1X -= 2;
+        cloud2X -= 2;
+        cloud1Rectangle = new Rectangle(cloud1X, cloud1Y, 128, 71);
+
+    }
+
+
+    private void animatePlayer() {
+        if (playerY == PLAYER_Y) {
+            counter++;
+            counter = counter % 8;
+            playerImage = playerImages[counter];
+        }
+        playerVelocityY += playerAccY;
+        playerY += playerVelocityY;
+        if (playerY >= PLAYER_Y) {
+            playerY = PLAYER_Y;
+            playerVelocityY = 0;
+            playerAccY = 0;
+        }
+        playerRectangle = new Rectangle(100, playerY, 72, 90);
+    }
+
 
     @Override
     public void keyTyped(KeyEvent e) {
-
+        System.out.println("KeyTyped");
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_SPACE && this.playerY == 450 - 45 - 90) {
-            this.playerVelocityY = -20;
-            this.playerAccY = 1;
+        System.out.println("KeyPressed");
+        if (this.playerY == PLAYER_Y) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_UP:
+                case KeyEvent.VK_NUMPAD8:
+                    this.playerVelocityY = -20;
+                    this.playerAccY = 1;
+                    break;
+                case KeyEvent.VK_DOWN:
+                case KeyEvent.VK_NUMPAD2:
+                    break;
+
+            }
+
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        System.out.println("KeyReleased");
     }
 
 
